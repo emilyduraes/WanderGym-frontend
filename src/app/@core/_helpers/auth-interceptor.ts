c7d0b40e31960969a8db8030d2aa52737cf20931
emilyduraes <emilyduraes@yahoo.com.br>
2024-01-09 17:02:42 +0000

    fix auth interceptor

--- src/app/@core/_helpers/auth-interceptor.ts
+++ src/app/@core/_helpers/auth-interceptor.ts
@@ -1,28 +1,44 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpHeaders } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { LoginService } from '../services/login.service';
+import { Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private loginService: LoginService) { }
+  private cache = new Map<string, HttpResponse<any>>();
+  private auth: any;
+
+  private loginUrl = "http://localhost:8080/wandergym/auth/login"
+
+
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.loginService.getToken();
 
-    if (this.loginService.isUserLoggedIn()) {
-      const authReq = req.clone({
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json',
-          'Authorization': `${token}`
+    // if its a login request, cache the response
+    if (req.url == this.loginUrl) {
+      return next.handle(req).pipe(
+        tap((res) => {
+          if (res instanceof HttpResponse) {
+            this.cache.set(req.url, res);
+          }
         })
-      });
-      return next.handle(authReq);
-    } else {
-      return next.handle(req);
+      )
     }
+
+    // set token to the request
+    const authRes = this.cache.get(this.loginUrl);
+    const token = authRes.body.login.responseObject.basicAuthorization;
+    const authReq = req.clone({
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `${token}`
+      })
+    });
+
+    return next.handle(authReq);
+
   }
+
 }
